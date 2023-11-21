9b1feea500824e0abe916e3c932f67141f12ab71
mmaksymko <maksymkomyna@gmail.com>
2023-11-21 22:39:35 +0200

    initial commit

---
+++ Back/src/main/java/ua/lpnu/security_system_simulator/SecuritySystemSimulatorApplication.java
@@ -0,0 +1,13 @@
+package ua.lpnu.security_system_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySystemSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySystemSimulatorApplication.class, args);
+	}
+
+}
---
+++ Back/src/test/java/ua/lpnu/Security/System/Simulator/SecuritySystemSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.lpnu.Security.System.Simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySystemSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
