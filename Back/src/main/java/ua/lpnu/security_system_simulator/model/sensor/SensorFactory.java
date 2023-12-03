0c1cfce149531948b7578623e1cd1edc303a645a
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-12-03 02:08:01 +0200

    [feature] Sensors factory created.

---
+++ Back/src/main/java/ua/lpnu/security_system_simulator/model/sensor/SensorFactory.java
@@ -0,0 +1,18 @@
+package ua.lpnu.security_system_simulator.model.sensor;
+
+import org.springframework.stereotype.Component;
+import ua.lpnu.security_system_simulator.model.event.EventType;
+
+@Component
+public class SensorFactory {
+    public Sensor createSensor(EventType event, int coverageArea) {
+        return switch(event){
+            case FIRE -> new FireSensor(coverageArea);
+            case FLOODING -> new FloodingSensor(coverageArea);
+            case GAS_LEAK -> new GasLeakSensor(coverageArea);
+            case MOTION -> new MotionSensor(coverageArea);
+            case OPENED_WINDOW -> new OpenedWindowSensor(coverageArea);
+            case null -> null;
+        };
+    }
+}
---
+++ Back/src/test/java/ua/lpnu/Security/System/Simulator/model/sensor/SensorFactoryTests.java
@@ -0,0 +1,59 @@
+package ua.lpnu.Security.System.Simulator.model.sensor;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ua.lpnu.security_system_simulator.model.event.EventType;
+import ua.lpnu.security_system_simulator.model.sensor.*;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = ua.lpnu.security_system_simulator.model.sensor.SensorFactory.class)
+public class SensorFactoryTests {
+    @Autowired
+    private SensorFactory sensorFactory;
+    @Test
+    public void sensorFactoryReturnsCorrectFireSensor(){
+        Assertions.assertEquals(new FireSensor(15), sensorFactory.createSensor(EventType.FIRE, 15));
+        Assertions.assertEquals(new FireSensor(Integer.MAX_VALUE), sensorFactory.createSensor(EventType.FIRE, Integer.MAX_VALUE));
+        Assertions.assertEquals(new FireSensor(Integer.MAX_VALUE-1), sensorFactory.createSensor(EventType.FIRE, Integer.MAX_VALUE-1));
+        Assertions.assertEquals(new FireSensor(1), sensorFactory.createSensor(EventType.FIRE, 1));
+    }
+    @Test
+    public void sensorFactoryReturnsCorrectFloodingSensor(){
+        Assertions.assertEquals(new FloodingSensor(15), sensorFactory.createSensor(EventType.FLOODING, 15));
+        Assertions.assertEquals(new FloodingSensor(Integer.MAX_VALUE), sensorFactory.createSensor(EventType.FLOODING, Integer.MAX_VALUE));
+        Assertions.assertEquals(new FloodingSensor(Integer.MAX_VALUE-1), sensorFactory.createSensor(EventType.FLOODING, Integer.MAX_VALUE-1));
+        Assertions.assertEquals(new FloodingSensor(1), sensorFactory.createSensor(EventType.FLOODING, 1));
+    }
+
+    @Test
+    public void sensorFactoryReturnsCorrectOpenedWindowSensor(){
+        Assertions.assertEquals(new OpenedWindowSensor(15), sensorFactory.createSensor(EventType.OPENED_WINDOW, 15));
+        Assertions.assertEquals(new OpenedWindowSensor(Integer.MAX_VALUE), sensorFactory.createSensor(EventType.OPENED_WINDOW, Integer.MAX_VALUE));
+        Assertions.assertEquals(new OpenedWindowSensor(Integer.MAX_VALUE-1), sensorFactory.createSensor(EventType.OPENED_WINDOW, Integer.MAX_VALUE-1));
+        Assertions.assertEquals(new OpenedWindowSensor(1), sensorFactory.createSensor(EventType.OPENED_WINDOW, 1));
+    }
+    @Test
+    public void sensorFactoryReturnsCorrectGasLeakSensor(){
+        Assertions.assertEquals(new GasLeakSensor(15), sensorFactory.createSensor(EventType.GAS_LEAK, 15));
+        Assertions.assertEquals(new GasLeakSensor(Integer.MAX_VALUE), sensorFactory.createSensor(EventType.GAS_LEAK, Integer.MAX_VALUE));
+        Assertions.assertEquals(new GasLeakSensor(Integer.MAX_VALUE-1), sensorFactory.createSensor(EventType.GAS_LEAK, Integer.MAX_VALUE-1));
+        Assertions.assertEquals(new GasLeakSensor(1), sensorFactory.createSensor(EventType.GAS_LEAK, 1));
+    }
+    @Test
+    public void sensorFactoryReturnsCorrectMotionSensor(){
+        Assertions.assertEquals(new MotionSensor(15), sensorFactory.createSensor(EventType.MOTION, 15));
+        Assertions.assertEquals(new MotionSensor(Integer.MAX_VALUE), sensorFactory.createSensor(EventType.MOTION, Integer.MAX_VALUE));
+        Assertions.assertEquals(new MotionSensor(Integer.MAX_VALUE-1), sensorFactory.createSensor(EventType.MOTION, Integer.MAX_VALUE-1));
+        Assertions.assertEquals(new MotionSensor(1), sensorFactory.createSensor(EventType.MOTION, 1));
+    }
+    @Test
+    public void sensorFactoryReturnsNullWhenParamIsNull(){
+        Assertions.assertNull(sensorFactory.createSensor(null, 15));
+    }
+}
