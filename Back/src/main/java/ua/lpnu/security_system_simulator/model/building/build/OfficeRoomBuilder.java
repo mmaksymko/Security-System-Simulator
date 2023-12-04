876f8e0cd50f13a4c7d59df189adc71e35194381
weroumi <ookk1405@gmail.com>
2023-12-04 18:47:24 +0200

    [feat] initial office room builder

--- Back/src/main/java/ua/lpnu/security_system_simulator/model/building/build/OfficeRoomBuilder.java
+++ Back/src/main/java/ua/lpnu/security_system_simulator/model/building/build/OfficeRoomBuilder.java
@@ -1,4 +1,36 @@
 package ua.lpnu.security_system_simulator.model.building.build;
 
-public class OfficeRoomBuilder {
+import ua.lpnu.security_system_simulator.model.building.Room;
+import ua.lpnu.security_system_simulator.model.sensor.Sensor;
+
+public class OfficeRoomBuilder implements RoomBuilder{
+    private Room result;
+    @Override
+    public void reset() {
+        this.result = new Room();
+    }
+    @Override
+    public void setArea(int area) {
+        result.setArea(area);
+    }
+    @Override
+    public void setRoomNumber(int roomNumber) {
+        result.setRoomNumber(roomNumber);
+    }
+    @Override
+    public void addWindow(int numberOfWindows) {
+        result.addWindows(numberOfWindows);
+    }
+    @Override
+    public void addDoor(int numberOfDoors) {
+        result.addDoors(numberOfDoors);
+    }
+    @Override
+    public void addSensor(Sensor sensor) {
+        result.addSensor(sensor);
+    }
+    @Override
+    public Room getResult() {
+        return null;
+    }
 }
