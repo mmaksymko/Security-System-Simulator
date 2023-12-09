63c44a1ab07ed0e8942f37df67e6d29a0bc03c1b
liuda <luda.min2003@gmail.com>
2023-12-09 16:57:03 +0200

    add buttons for starting/pausing/continuing of simulation

--- Front/src/components/Simulation.tsx
+++ Front/src/components/Simulation.tsx
@@ -1,11 +1,49 @@
+import React, { useState } from "react";
 import ChangeFloorButton from "./ChangeFloorButton";
+import StartSimulationButton from "./StartSimulationButton";
+import ContinueSimulationButton from "./ContinueSimulationButton";
 import styles from "./Simulation.module.css";
 
 function Simulation() {
+  const [simulationState, setSimulationState] = useState("start");
+
+  const handleStartClick = () => {
+    setSimulationState("continue");
+  };
+
+  const handleStopClick = () => {
+    setSimulationState("stop");
+  };
+  const handleContinueClick = () => {
+    setSimulationState("continue");
+  };
+
+  const resetSimulation = () => {
+    setSimulationState("start");
+  };
+
   return (
     <div className={styles.container}>
       <ChangeFloorButton />
-      <p style={{ color: "#000" }}>simulation is here</p>
+      {simulationState === "start" && (
+        <StartSimulationButton onClick={handleStartClick} />
+      )}
+      {simulationState === "stop" && (
+        <ContinueSimulationButton onClick={handleContinueClick} />
+      )}
+      <div className={styles.simulation} onClick={handleStopClick}>
+        <p>Floor plan and simulation here</p>
+      </div>
+      {simulationState === "continue" && (
+        <p className={styles.message}>
+          Press on the floor map to stop simulation
+        </p>
+      )}
+      {simulationState === "stop" && (
+        <p className={styles.message}>
+          Press on the floor map to continue simulation
+        </p>
+      )}
     </div>
   );
 }
