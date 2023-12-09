1b83e2be8230960b6ee08ea3956caca4bdae60d9
weroumi <ookk1405@gmail.com>
2023-12-09 20:03:45 +0200

    [ref] full director again. help..,

--- Back/src/main/java/ua/lpnu/security_system_simulator/model/building/roomBuilder/RoomDirector.java
+++ Back/src/main/java/ua/lpnu/security_system_simulator/model/building/roomBuilder/RoomDirector.java
@@ -11,23 +11,11 @@ public class RoomDirector {
     public RoomDirector(int floor){
         this.roomNumberCounter = floor * 100;
     }
-    public void constructBigApartmentRoom(Builder builder) throws Exception{
+    public void constructApartmentRoom(Builder builder) throws Exception{
         builder.setRoomType(RoomType.APARTMENT_ROOM);
         builder.setWidth(2);
         builder.setLength(2);
         builder.setWindows(2);
-        builder.setDoors(1);
-        builder.setRoomNumber(roomNumberCounter++);
-        builder.addSensor(new FireSensor(2));
-        builder.addSensor(new FloodingSensor(2));
-        builder.addSensor(new GasLeakSensor(2));
-        builder.addSensor(new MotionSensor(1));
-    }
-    public void constructSmallApartmentRoom(Builder builder) throws Exception{
-        builder.setRoomType(RoomType.APARTMENT_ROOM);
-        builder.setWidth(1);
-        builder.setLength(2);
-        builder.setWindows(1);
         builder.setDoors(1);
         builder.setRoomNumber(roomNumberCounter++);
         builder.addSensor(new FireSensor(2));
@@ -48,7 +36,7 @@ public class RoomDirector {
     }
     public void constructApartmentKitchen(Builder builder) throws Exception{
         builder.setRoomType(RoomType.KITCHEN);
-        builder.setWidth(2);
+        builder.setWidth(3);
         builder.setLength(1);
         builder.setWindows(1);
         builder.setDoors(1);
@@ -60,9 +48,9 @@ public class RoomDirector {
     }
     public void constructBigOfficeRoom(Builder builder) throws Exception{
         builder.setRoomType(RoomType.OFFICE_ROOM);
-        builder.setWidth(2);
-        builder.setLength(4);
-        builder.setWindows(6);
+        builder.setWidth(3);
+        builder.setLength(2);
+        builder.setWindows(5);
         builder.setDoors(2);
         builder.setRoomNumber(roomNumberCounter++);
         builder.addSensor(new FireSensor(2));
@@ -84,7 +72,7 @@ public class RoomDirector {
     public void constructOfficeRestroom(Builder builder)throws Exception{
         builder.setRoomType(RoomType.OFFICE_RESTROOM);
         builder.setWidth(2);
-        builder.setLength(3);
+        builder.setLength(1);
         builder.setDoors(1);
         builder.setRoomNumber(roomNumberCounter++);
         builder.addSensor(new FireSensor(2));
@@ -94,9 +82,9 @@ public class RoomDirector {
     }
     public void constructOfficeKitchen(Builder builder) throws Exception{
         builder.setRoomType(RoomType.KITCHEN);
-        builder.setWidth(2);
-        builder.setLength(3);
-        builder.setWindows(4);
+        builder.setWidth(1);
+        builder.setLength(2);
+        builder.setWindows(2);
         builder.setDoors(1);
         builder.setRoomNumber(roomNumberCounter++);
         builder.addSensor(new FireSensor(2));
