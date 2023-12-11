c54476ae97fb3b2122d33f6523fe48236938014f
AnnaHuk05 <annavonhuk@gmail.com>
2023-12-11 23:04:00 +0200

    emoji show added

--- Front/src/components/Rooms/Bedroom.tsx
+++ Front/src/components/Rooms/Bedroom.tsx
@@ -1,4 +1,5 @@
 import "./RoomsStyle.scss";
+import React, { useState } from "react";
 
  type Props = {
   roomType : string
@@ -12,10 +13,20 @@ function Bedroom({roomType: bedType, width, height, label} : Props) {
     gridColumn: `span ${width}`,
     gridRow: `span ${height}`,
   };
-
+  const [emojiPosition, setEmojiPosition] = useState({
+    top: Math.floor(Math.random() * height *(Math.floor(Math.random() * 45)  )),
+    left: Math.floor(Math.random() * width *(Math.floor(Math.random() * 45))),
+  });
+  const smileyStyle = {
+    top: `${emojiPosition.top }%`,
+    left: `${emojiPosition.left}%`,
+    position: "absolute" as const,
+    zIndex: 2,
+  };
   return <div className="Components Bedroom" id={`${bedType}`} style={style} >
       <div className="Door" />
       <div className="Label">{label}</div>
+      <div className="Smiley" style={smileyStyle}>😊</div>
   </div>;
 }
 
