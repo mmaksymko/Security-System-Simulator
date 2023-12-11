18963f33a2a668c9a962491c2ef98d47f38a483e
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-12-11 17:53:21 +0200

    [feat] new event type

--- Back/src/main/java/ua/lpnu/security_system_simulator/model/event/EventFactory.java
+++ Back/src/main/java/ua/lpnu/security_system_simulator/model/event/EventFactory.java
@@ -7,19 +7,28 @@ import java.util.Date;
 
 @Component
 public class EventFactory {
-    public Event createEvent(EventType eventType, EventTarget eventTarget, DangerLevel dangerLevel) throws InvalidParameterException {
-        return this.createEvent(eventType, eventTarget, dangerLevel, new Date());
-    }
+    public Event createEvent(EventType eventType, EventTarget eventTarget, DangerLevel dangerLevel) throws
+            InvalidParameterException, InterruptedException {
+        if (eventType == null || eventTarget == null || dangerLevel == null) {
+            throw new InvalidParameterException("Some parameters are null");
+        }
 
-    public Event createEvent(EventType eventType, EventTarget eventTarget, DangerLevel dangerLevel, Date happenedAt) throws InvalidParameterException {
+        return switch (eventType) {
+            case OPENED_WINDOW, OPENED_DOOR -> new OpenableEvent(eventType, eventTarget, dangerLevel, new Date());
+            case SIMULATION_START -> new SimulationEvent(eventType, eventTarget, dangerLevel, new Date());
+            default -> new RoomEvent(eventType, eventTarget, dangerLevel, new Date());
+        };
+    }
+    public Event createEvent(EventType eventType, EventTarget eventTarget, DangerLevel dangerLevel, Date happenedAt, boolean reaction) throws InvalidParameterException {
         if(eventType == null || eventTarget == null || dangerLevel == null){
             throw new InvalidParameterException("Some parameters are null");
         }
 
         return switch(eventType) {
-            case OPENED_WINDOW, OPENED_DOOR -> new OpenableEvent(eventType, eventTarget, dangerLevel, happenedAt);
-            case SIMULATION_START -> new SimulationEvent(eventType, eventTarget, dangerLevel, happenedAt);
-            default -> new RoomEvent(eventType, eventTarget, dangerLevel, happenedAt);
+            case OPENED_WINDOW, OPENED_DOOR -> new OpenableEvent(eventType, eventTarget, dangerLevel, happenedAt, reaction);
+            case FIRE, FLOODING, MOTION, GAS_LEAK -> new RoomEvent(eventType, eventTarget, dangerLevel, happenedAt, reaction);
+            case SIMULATION_START -> new SimulationEvent(eventType, eventTarget, dangerLevel, happenedAt, reaction);
+            default -> new ReactionEvent(eventType, eventTarget, dangerLevel, happenedAt, reaction);
         };
     }
 }
