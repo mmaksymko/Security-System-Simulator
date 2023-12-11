11a3819aa42bad905c951136108142bc6a05fe42
maksymkomyna@gmail.com <maksymkomyna@gmail.com>
2023-12-11 04:57:13 +0200

    [feat] new function

--- Back/src/main/java/ua/lpnu/security_system_simulator/model/building/Room.java
+++ Back/src/main/java/ua/lpnu/security_system_simulator/model/building/Room.java
@@ -4,16 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import ua.lpnu.security_system_simulator.model.event.Event;
-import ua.lpnu.security_system_simulator.model.event.EventTarget;
-import ua.lpnu.security_system_simulator.model.event.EventType;
+import ua.lpnu.security_system_simulator.model.event.*;
 import ua.lpnu.security_system_simulator.model.sensor.Sensor;
 import ua.lpnu.security_system_simulator.model.system.EventLog;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Document
 public class Room implements BuildingComponent, EventTarget {
@@ -142,6 +137,10 @@ public class Room implements BuildingComponent, EventTarget {
         if (logs.isEmpty())
             logs.add(new EventLog());
         return logs.getLast();
+    }
+
+    public void createNewLog() {
+        logs.add(new EventLog());
     }
 
     private void setLogs(List<EventLog> logs) {
