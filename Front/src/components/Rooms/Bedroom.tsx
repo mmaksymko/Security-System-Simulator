c5369e415c61a90fdc24ab3d1a7810060b306175
AnnaHuk05 <annavonhuk@gmail.com>
2023-12-12 06:26:41 +0200

    add handle in bedroom

--- Front/src/components/Rooms/Bedroom.tsx
+++ Front/src/components/Rooms/Bedroom.tsx
@@ -13,21 +13,30 @@ function Bedroom({roomType: bedType, width, height, label} : Props) {
     gridColumn: `span ${width}`,
     gridRow: `span ${height}`,
   };
+  const handleRoomClick = () => {
+    // Генеруємо нові координати смайлика при кліці на кімнату
+    setEmojiPosition({
+      top: Math.floor(Math.random() * height) + 1,
+      left: Math.floor(Math.random() * width) + 1,
+    });
+  };
   const [emojiPosition, setEmojiPosition] = useState({
-    top: Math.floor(Math.random() * height *(Math.floor(Math.random() * 45)  )),
-    left: Math.floor(Math.random() * width *(Math.floor(Math.random() * 45))),
+    top: Math.floor(Math.random() * height *(Math.floor(Math.random() * 100)  )),
+    left: Math.floor(Math.random() * width *(Math.floor(Math.random() * 70))),
   });
   const smileyStyle = {
-    top: `${emojiPosition.top }%`,
-    left: `${emojiPosition.left}%`,
+    top: `${emojiPosition.top * 100 / height}%`,
+    left: `${emojiPosition.left * 100 / width}%`,
     position: "absolute" as const,
     zIndex: 2,
   };
-  return <div className="Components Bedroom" id={`${bedType}`} style={style} >
+ 
+  return <div className="Components Bedroom" id={`${bedType}`} style={style}onClick={handleRoomClick}>
       <div className="Door" />
       <div className="Label">{label}</div>
       <div className="Smiley" style={smileyStyle}>😊</div>
-  </div>;
+      </div>;
+
 }
 
 export default Bedroom;
